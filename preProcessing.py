8080a6bc2b1e0526d531c4caa1fc0aad15a44e1e
Bas Vogelzang <bas.vogelzang95@gmail.com>
2018-01-05 22:13:52 +0100

    try to train model

--- preProcessing.py
+++ preProcessing.py
@@ -1,12 +1,28 @@
 import csv
+from sklearn.metrics import accuracy_score
 import pandas as pd
 from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.datasets import make_multilabel_classification
+# using binary relevance
+#from skmultilearn.problem_transform import BinaryRelevance
+from sklearn.naive_bayes import GaussianNB
+from skmultilearn.problem_transform import BinaryRelevance
+from sklearn.svm import SVC
+
+
 
 seperate_genre=['Action','Adventure','Animation','Biography','Comedy','Crime','Drama','Fantasy','Family','History','Horror','Music','Musical','Mystery','Romance','Sci-Fi','Sport','Thriller','War','Western']
 
 def readData(name):
     data = pd.read_csv(name)
     return data
+
+
+def extract_features(text):
+    features = []
+    features.append(1)
+    features.append(2)
+    return features
 
 def getGenres(data):
     genres = data["Genre"]
@@ -32,27 +48,30 @@ def read_data(name):
     return descriptions, genres
 '''
 data = readData("IMDB-Movie-Data.csv")
+
 genresN = getGenres(data)
-binary = MultiLabelBinarizer().fit_transform(genresN)
-features = []
+descriptions = data["Description"]
+print descriptions[1]
+labels = MultiLabelBinarizer().fit_transform(genresN)
+features = list(map(extract_features, descriptions))
+print features
+''''
+classifier = BinaryRelevance(classifier = SVC(), require_dense = [False, True])
+# train
+classifier.fit(features, labels)
+predictions = classifier.predict(features)
+for p in predictions:
+    print p
+'''
+#features = list(map(extract_features, train_data.data))
 
-X = features.data
-Y = genresN.target
+# initialize binary relevance multi-label classifier
+# with a gaussian naive bayes base classifier
+classifier = BinaryRelevance(GaussianNB())
+# train
+classifier.fit(features, labels)
+# predict
+predictions = classifier.predict(features)
+acc = accuracy_score(labels, predictions)
 
-print binary[0:5]
-
-
-
-
-
-
-
-
-
-
-
-#d, g = read_data('IMDB-Movie-Data.csv')
-#str = g[1]
-#print str
-#[x.strip() for x in str.split(',')]
-
+print acc
